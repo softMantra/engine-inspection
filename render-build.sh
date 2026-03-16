6b1377f80618527229856a30e9253b3cec24eaa4
amne8828-sketch <amne8828@gmail.com>
2026-03-16 10:58:48 +0530

    model saved in google drive

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
 
-# exit on error
 set -o errexit
 
 # Install required dependencies
@@ -9,10 +7,8 @@ pip install -r requirements.txt
 # Create models directory if it doesn't exist
 mkdir -p models
 
-# Replace this with your actual Google Drive File ID
-# For example, if your link is https://drive.google.com/file/d/1XyZ_abc123.../view
-# The ID is 1XyZ_abc123...
-GDRIVE_FILE_ID="YOUR_GOOGLE_DRIVE_FILE_ID_HERE"
+
+GDRIVE_FILE_ID="https://drive.google.com/file/d/1NGPQ8HbtE9Ioc5_S6fPDaKjJ0nGklGx-/view?usp=drive_link"
 
 # Check if model already exists (optional, saves time on rebuilds)
 if [ ! -f "models/best.pth" ]; then
